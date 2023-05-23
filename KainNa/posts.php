bf83a5f75b198be66e489a377acfa8af81f04207
BryanJayHizole <bryanhizole2001@gmail.com>
2023-05-24 00:35:29 +0800

    fix the blog page

---
+++ KainNa/posts.php
@@ -0,0 +1,16 @@
+<?php
+$posts = [
+    [
+        'title' => 'Cooking recipe Delicious',
+        'image' => 'images/blog-05.jpg',
+        'content' => 'This is the content of my first blog post.',
+        'author' => 'John Doe'
+    ],
+    [
+        'title' => 'Pizza is prepared fresh',
+        'image' => 'images/blog-06.jpg',
+        'content' => 'This is another blog post.',
+        'author' => 'Jane Smith'
+    ]
+];
+?>
